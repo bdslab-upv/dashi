72f93dcb44586c10315b7279792ba95b11ef444a
Ángel Sánchez <ansan12a@upv.es>
2024-12-12 11:42:00 +0100

    Preparing for deployment

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dashi',
-    version='0.1',
+    name="dashi",  # Replace with your library's name
+    version="0.1.0",  # Initial version
+    author="Carlos Sáez Silvestre, David Fernández Narro, Ángel Sánchez García",
+    author_email="",
+    description="A brief description of your library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdslab-upv/dashi",
     packages=find_packages(),
-    install_requires=[
-        '',
+    include_package_data=False,  # Include non-Python files listed in MANIFEST.in
+    install_requires=[  # Add your dependencies here
+        # Example: "numpy>=1.21.0",
+        "numpy>=1.26.2",
+        "pandas>=2.2.3",
+        "plotly==5.18.0",
+        "sklearn==",
+        "dateutil",
+        "tqdm",
+        "prince",
+
     ],
-    description='A description of my library',
-    author='Your Name',
-    author_email='youremail@example.com',
-    url='https://github.com/yourusername/my_library',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Adjust if not MIT
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.7",  # Minimum Python version required
+    keywords="dashi, datashift",  # Keywords for PyPI search
+    project_urls={  # Additional URLs
+        "Documentation": "https://github.com/yourusername/dashi/docs",
+        "Source": "https://github.com/bdslab-upv",
+        "Tracker": "https://github.com/yourusername/dashi/issues",
+    },
 )
