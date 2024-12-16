21a3b322136ac29cc761269be727839e387b0306
David <dfernar@upv.edu.es>
2024-12-16 10:50:17 +0000

    Update dependencies setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
         "sklearn==1.5.1",
         "dateutil>=2.8.2",
         "tqdm>=4.66.5",
-        "prince>=0.14.0"
+        "prince>=0.14.0",
+        "matplotlib>=3.10.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
